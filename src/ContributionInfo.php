96e3ea2b7cd866eec29323f3a731e357ba5fc474
Pieplu <alexis.pieplu@gmail.com>
2013-11-11 20:54:24 -0500

    refactor class UserInfo to ContributionInfo

--- src/ContributionInfo.php
+++ src/ContributionInfo.php
@@ -3,14 +3,24 @@
  * User: alexis
  */
 
-class UserInfo {
+class ContributionInfo {
 
-
+    private $userName;
+    private $website;
     private $pagesId;
     private $oldVersion;
     private $userVersion;
     private $usertimestamp;
 
+
+    public function getUserName()
+    {
+        return $this->userName;
+    }
+    public function getWebSite()
+    {
+        return $this->website;
+    }
     public function getOldVersion()
     {
         return $this->oldVersion;
@@ -28,7 +38,14 @@ class UserInfo {
         return $this->usertimestamp;
     }
 
-
+    public function setUserName($userName)
+    {
+        $this->userName = $userName;
+    }
+    public function setWebsite($webSite)
+    {
+        $this->webSite = $webSite;
+    }
     public function setPagesId($pages)
     {
         $this->pagesId = $pages;
