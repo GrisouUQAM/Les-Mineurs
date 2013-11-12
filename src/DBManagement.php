f3e30846e94ef825905d0dee5c2f709e41e8abdf
dmenard <d.menard@live.com>
2013-11-11 21:53:08 -0500

    Creation de méthode d'insertion dans la BD d'une contribution et de la vérification de la présence d'un contributeur et de son insertion dans la BD s'il n'est pas présent.

--- src/DBManagement.php
+++ src/DBManagement.php
@@ -1,23 +1,25 @@
 <?php
 
 include_once( dirname(__FILE__) . '/DBConnection.php');
-include_once( dirname(__FILE__) . '/UserInfo.php');
+include_once( dirname(__FILE__) . '/ContributionInfo.php');
 
-public function retrieveContributionID($contributorID){
-  $result = $bdd->query('SELECT page_id FROM contributions where ID ='.$contributorID);
+public function retrieveContributionID($contributorID,$website){
+  $result = $bdd->query('SELECT page_id FROM contributions where ID ='.$contributorID' and website = '.$website);
   $contributionsID;
-  while ($contributionsID =result->fetch()){ }
+  while ($contributionsID =result->fetch())
+{ }
   return $contributionsID;
 }
 
 public function insertIfContributionNotPresent($contributorID,$UserInfo){
-
-  $contributionsID = retrieveContributionID($contributorID)
   
+  $contributionsID = retrieveContributionID($contributorID);
+    
   $contributionIDToInsert = $UserInfo.getPagesId();
   $contributionOldVersionToInsert = $UserInfo.getOldVersion();  
   $contributionUserVersionToInsert = $UserInfo.getUserVersion();  
-  $contributionUsertimestamp = $UserInfo.getUsertimestamp();  
+  $contributionUsertimestampToInsert = $UserInfo.getUsertimestamp();
+  $contributionWebsiteToInsert = $UserInfo.getWebsite(); 
   
   foreach ($contributionsID as $IDvalue){
     if($IDvalue == $contributionIDToInsert){
@@ -25,10 +27,15 @@ public function insertIfContributionNotPresent($contributorID,$UserInfo){
     }
   }
   
-  
-  
-  $bdd->exec('INSERT INTO contributions(nom, possesseur, console, prix, nbre_joueurs_max, commentaires) VALUES(\'Battlefield 1942\', \'Patrick\', \'PC\', 45, 50, \'2nde guerre mondiale\')');
+  $bdd->exec("INSERT INTO contributions('ID',page_id','rev_id','parent_id','time','website') VALUES('".$contributorID."','".$contributionIDToInsert."','".$contributionUserVersionToInsert"','".$contributionOldVersionToInsert."','".$contributionUsertimestampToInsert."','".$contributionWebsiteToInsert."')");
   
 }
 
+public function verifyContributorIDandCreateIfNotPresent($username){
+    $result = $bdd->query('SELECT ID FROM contributor where username ='.$username);
+    $row_count = $result->rowCount();
+    if($row_count==0){
+      $bdd->exec("INSERT INTO contributor('username') VALUES('".$username."')");
+    }
+}
 ?>
