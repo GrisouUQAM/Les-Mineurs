a61bf25511d538144b694888dac288440625d91c
esimard <etienne.radial@gmail.com>
2013-11-11 20:16:03 -0500

    Script de creation de base de donnees
    Voici le script pour creer la base de donnee ainsi que ses 2 tables.

---
+++ src/wiki_contrib.sql
@@ -0,0 +1,55 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.4
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Nov 12, 2013 at 01:12 AM
+-- Server version: 5.6.12-log
+-- PHP Version: 5.4.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `wiki_contrib`
+--
+CREATE DATABASE IF NOT EXISTS `wiki_contrib` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+USE `wiki_contrib`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contributeurs`
+--
+
+CREATE TABLE IF NOT EXISTS `contributeurs` (
+  `ID` int(11) NOT NULL,
+  `nom_de_contributeur` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contributions`
+--
+
+CREATE TABLE IF NOT EXISTS `contributions` (
+  `ID` int(11) NOT NULL,
+  `page_id` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `rev_id` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `parent_id` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `provenance` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
