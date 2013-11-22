7d8f9dc66696e7371b948d6df5a29347a6dea2f4
Pieplu <alexis.pieplu@gmail.com>
2013-11-22 10:23:22 -0500

    phpUnit instaler (voir mails pour liens)

---
+++ test/DBManagementTest.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alexis
+ * Date: 2013-11-22
+ * Time: 10:14
+ */
+
+class DBManagementTest extends PHPUnit_Framework_TestCase {
+    function __call($name, $arguments)
+    {
+        // TODO: Implement __call() method.
+    }
+
+    public static function __callStatic($name, $arguments)
+    {
+        // TODO: Implement __callStatic() method.
+    }
+
+    function __construct()
+    {
+        // TODO: Implement __construct() method.
+    }
+
+    function __destruct()
+    {
+        // TODO: Implement __destruct() method.
+    }
+
+    function __get($name)
+    {
+        // TODO: Implement __get() method.
+    }
+
+    function __set($name, $value)
+    {
+        // TODO: Implement __set() method.
+    }
+
+    function __isset($name)
+    {
+        // TODO: Implement __isset() method.
+    }
+
+    function __unset($name)
+    {
+        // TODO: Implement __unset() method.
+    }
+
+    function __sleep()
+    {
+        // TODO: Implement __sleep() method.
+    }
+
+    function __wakeup()
+    {
+        // TODO: Implement __wakeup() method.
+    }
+
+    function __toString()
+    {
+        // TODO: Implement __toString() method.
+    }
+
+    function __invoke()
+    {
+        // TODO: Implement __invoke() method.
+    }
+
+    static function __set_state($an_array)
+    {
+        // TODO: Implement __set_state() method.
+    }
+
+    function __clone()
+    {
+        // TODO: Implement __clone() method.
+    }
+
+
+}
+ 
