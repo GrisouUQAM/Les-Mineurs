6a6a7210551ed973aacab0d940ea2fa99ae90f7e
Pieplu <alexis.pieplu@gmail.com>
2013-11-22 12:53:27 -0500

    test pas encore fonctionel, on y est presque. Documentation à jour

--- Documentation.md
+++ Documentation.md
@@ -12,7 +12,9 @@ Test Unitaire
 
 [PhpUnit][1]  |  [PhpStorm][2]
 
+(Ps: pour mac osx, n'oubliez pas d'installer [PEAR][3] avant)
 
 
   [1]: http://phpunit.de/manual/current/en/installation.html
   [2]: http://confluence.jetbrains.com/display/PhpStorm/PHPUnit+Installation+via+Composer+in+PhpStorm
+  [3]: http://www.coolestguidesontheplanet.com/downtown/installing-pear-osx-109-mavericks-and-osx108107
--- test/DBManagementTest.php
+++ test/DBManagementTest.php
@@ -1,81 +1,17 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: alexis
- * Date: 2013-11-22
- * Time: 10:14
- */
 
 class DBManagementTest extends PHPUnit_Framework_TestCase {
-    function __call($name, $arguments)
+
+
+    /**
+     * @Des
+     */
+    function compareUserIfInTableTest1()
     {
-        // TODO: Implement __call() method.
+        $this->assertFalse(compareUserIfInTable('y76jnn'));
     }
 
-    public static function __callStatic($name, $arguments)
-    {
-        // TODO: Implement __callStatic() method.
-    }
 
-    function __construct()
-    {
-        // TODO: Implement __construct() method.
-    }
-
-    function __destruct()
-    {
-        // TODO: Implement __destruct() method.
-    }
-
-    function __get($name)
-    {
-        // TODO: Implement __get() method.
-    }
-
-    function __set($name, $value)
-    {
-        // TODO: Implement __set() method.
-    }
-
-    function __isset($name)
-    {
-        // TODO: Implement __isset() method.
-    }
-
-    function __unset($name)
-    {
-        // TODO: Implement __unset() method.
-    }
-
-    function __sleep()
-    {
-        // TODO: Implement __sleep() method.
-    }
-
-    function __wakeup()
-    {
-        // TODO: Implement __wakeup() method.
-    }
-
-    function __toString()
-    {
-        // TODO: Implement __toString() method.
-    }
-
-    function __invoke()
-    {
-        // TODO: Implement __invoke() method.
-    }
-
-    static function __set_state($an_array)
-    {
-        // TODO: Implement __set_state() method.
-    }
-
-    function __clone()
-    {
-        // TODO: Implement __clone() method.
-    }lw
 
 
 }
