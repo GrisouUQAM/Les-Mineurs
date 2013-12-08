6ac98cbc8a4d15f878066cd489df41294f0f0b51
esimard <etienne.radial@gmail.com>
2013-12-08 17:36:08 -0500

    standardisation pour select et insert
    fait pour contirbution...

--- src/DBManagement.php
+++ src/DBManagement.php
@@ -17,11 +17,24 @@ class DBManagement {
   
   public static function compareContributionIfInTable($uneContrib, $dataBase) {
     
-    $result =  $dataBase -> prepare('SELECT * FROM contributions WHERE ID ='  . $uneContrib->getUserID() . ' AND timestamp =' . $uneContrib->getPagesID() . 'AND website =' . $uneContrib->getWebSite());
-    $result->execute();
-    $row_count = $result -> rowCount();
-    return !($row_count == 0);
+//   $result =  $dataBase -> prepare('SELECT * FROM contributions WHERE ID ='  . $uneContrib->getUserID() . ' AND timestamp =' . $uneContrib->getPagesID() . 'AND website =' . $uneContrib->getWebSite());
+//    $result->execute();
+//    $row_count = $result -> rowCount();
+//    return !($row_count == 0);
+
+      $uneContribPID = $uneContrib->getPagesID();
+      $uneContribUID = $uneContrib->getUserID();
+      $uneContribWEB = $uneContrib->getWebSite();
+
+      $result = $dataBase -> query("SELECT * FROM contributions WHERE page_id='$uneContribPID' AND ID='$uneContribUID' AND website='$uneContribWEB'");
+      if(!$result){
+          print_r($dataBase->errorInfo());
+      }
+      $count = $result->rowCount();
+      return ($count>0);
   }
+
+
   
   
 /**********************************************************
@@ -47,7 +60,17 @@ class DBManagement {
             $contributorID = $uneContrib->getUserID();
 
 
-        $dataBase -> exec("INSERT INTO contributions('ID','page_id','rev_id','parent_id','time','website') VALUES('" . $contributorID . "','" . $contributionIDToInsert . "','" . $contributionUserVersionToInsert . "','" . $contributionOldVersionToInsert . "','" . $contributionUsertimestampToInsert . "','" . $contributionWebsiteToInsert . "')");
+
+        $result = $dataBase ->prepare("INSERT INTO talk(ID,page_id,rev_id,parent_id,time,website) VALUES(:contributorID,:contributionIDToInsert,:contributionUserVersionToInsert,:contributionOldVersionToInsert,:contributionUsertimestampToInsert,:contributionWebsiteToInsert)");
+        $result->execute(array(':contributorID' => $contributorID, ':contributionIDToInsert' => $contributionIDToInsert, ':contributionUserVersionToInsert' => $contributionUserVersionToInsert, ':contributionOldVersionToInsert' => $contributionOldVersionToInsert, ':contributionUsertimestampToInsert' => $contributionUsertimestampToInsert, ':contributionWebsiteToInsert' => $contributionWebsiteToInsert));
+        if(!$result){
+            print_r($dataBase->errorInfo());
+        }
+        $count = $result->rowCount();
+        return ($count>0);
+
+
+        //$dataBase -> exec("INSERT INTO contributions('ID','page_id','rev_id','parent_id','time','website') VALUES('" . $contributorID . "','" . $contributionIDToInsert . "','" . $contributionUserVersionToInsert . "','" . $contributionOldVersionToInsert . "','" . $contributionUsertimestampToInsert . "','" . $contributionWebsiteToInsert . "')");
       
       
     }      
