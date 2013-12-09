a42005d16dd7d2f69859bbdcc9d4cafc3edc4567
esimard <etienne.radial@gmail.com>
2013-12-08 19:36:46 -0500

    Script SQL sans donnees
    Ca va mieux pour les fresh install ;)

--- src/wiki_contrib.sql
+++ src/wiki_contrib.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Dec 09, 2013 at 12:12 AM
+-- Generation Time: Dec 09, 2013 at 12:34 AM
 -- Server version: 5.6.12-log
 -- PHP Version: 5.4.12
 
@@ -38,14 +38,6 @@ CREATE TABLE IF NOT EXISTS `contributions` (
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
--- Dumping data for table `contributions`
---
-
-INSERT INTO `contributions` (`ID`, `page_id`, `rev_id`, `parent_id`, `contrib_time`, `website`) VALUES
-(1, '3109537', '30052958', '29737862', '2008-05-27 19:58:19', 'fr.wikipedia.org'),
-(3, '146692', '1436002', '1342924', '2005-01-21 17:37:04', 'fr.wikipedia.org');
-
 -- --------------------------------------------------------
 
 --
@@ -56,16 +48,7 @@ CREATE TABLE IF NOT EXISTS `contributor` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `contributor_username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=4 ;
-
---
--- Dumping data for table `contributor`
---
-
-INSERT INTO `contributor` (`ID`, `contributor_username`) VALUES
-(1, 'victor'),
-(2, 'etienne'),
-(3, 'fernando');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -79,22 +62,6 @@ CREATE TABLE IF NOT EXISTS `talk` (
   `page_id` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `rev_id` varchar(255) CHARACTER SET utf8 NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Dumping data for table `talk`
---
-
-INSERT INTO `talk` (`ID`, `website`, `page_id`, `rev_id`) VALUES
-(1, 'fr.wikipedia.org', '511134', '21392592'),
-(1, 'fr.wikipedia.org', '511134', '17355651'),
-(1, 'fr.wikipedia.org', '108426', '14349484'),
-(1, 'fr.wikipedia.org', '959857', '13697420'),
-(1, 'fr.wikipedia.org', '959857', '13697413'),
-(1, 'fr.wikipedia.org', '959857', '13696868'),
-(1, 'fr.wikipedia.org', '959857', '13696821'),
-(1, 'fr.wikipedia.org', '959857', '13686857'),
-(1, 'fr.wikipedia.org', '959857', '13686516'),
-(1, 'fr.wikipedia.org', '959857', '13686469');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
